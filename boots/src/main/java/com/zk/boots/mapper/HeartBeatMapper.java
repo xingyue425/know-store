3ce40380c73827f88be447832c90fccb6e82a966
xingyue425 <2006429019zk@163.com>
2020-01-10 16:38:38 +0800

    add log junit mybatis test.

--- boots/src/main/java/com/zk/boots/mapper/HeartBeatMapper.java
+++ boots/src/main/java/com/zk/boots/mapper/HeartBeatMapper.java
@@ -1,4 +1,9 @@
 package com.zk.boots.mapper;
 
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface HeartBeatMapper {
+
+    public Integer dbHit();
 }
--- boots/src/test/java/com/zk/boots/BootsApplicationTests.java
+++ boots/src/test/java/com/zk/boots/BootsApplicationTests.java
@@ -1,13 +1,23 @@
 package com.zk.boots;
 
+import com.zk.boots.mapper.HeartBeatMapper;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class BootsApplicationTests {
 
+  @Autowired
+  private HeartBeatMapper heartBeatMapper;
+
   @Test
   void contextLoads() {
+    int result=heartBeatMapper.dbHit();
+    System.out.println(result);
   }
 
 
